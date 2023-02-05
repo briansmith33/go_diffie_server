69c2f667af42b0915d58fb86f381ae43d4eb42d4
Brian <Brianmatthewsmith33@outlook.com>
2023-02-05 15:55:35 -0800

    cleanup

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module shell_server
 
 go 1.17
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"crypto/rand"
 	"crypto/sha256"
 	"encoding/hex"
+	"github.com/joho/godotenv"
 	"fmt"
 	"io"
 	"math/big"
@@ -14,18 +15,20 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"log"
 )
 
 const (
 	HEADER_LENGTH = 1024
 	CONNECT       = 0
-	CHANGE_DIR    = 1
-	COMMAND       = 2
-	RESPONSE      = 3
+	MESSAGE       = 1
+	RESPONSE      = 2
+	ERROR         = 3
+	DISCONNECT    = 4
 )
 
 var (
-	prime = []byte("0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AAAC42DAD33170D04507A33A85521ABDF1CBA64ECFB850458DBEF0A8AEA71575D060C7DB3970F85A6E1E4C7ABF5AE8CDB0933D71E8C94E04A25619DCEE3D2261AD2EE6BF12FFA06D98A0864D87602733EC86A64521F2B18177B200CBBE117577A615D6C770988C0BAD946E208E24FA074E5AB3143DB5BFCE0FD108E4B82D120A92108011A723C12A787E6D788719A10BDBA5B2699C327186AF4E23C1A946834B6150BDA2583E9CA2AD44CE8DBBBC2DB04DE8EF92E8EFC141FBECAA6287C59474E6BC05D99B2964FA090C3A2233BA186515BE7ED1F612970CEE2D7AFB81BDD762170481CD0069127D5B05AA993B4EA988D8FDDC186FFB7DC90A6C08F4DF435C93402849236C3FAB4D27C7026C1D4DCB2602646DEC9751E763DBA37BDF8FF9406AD9E530EE5DB382F413001AEB06A53ED9027D831179727B0865A8918DA3EDBEBCF9B14ED44CE6CBACED4BB1BDB7F1447E6CC254B332051512BD7AF426FB8F401378CD2BF5983CA01C64B92ECF032EA15D1721D03F482D7CE6E74FEF6D55E702F46980C82B5A84031900B1C9E59E7C97FBEC7E8F323A97A7E36CC88BE0F1D45B7FF585AC54BD407B22B4154AACC8F6D7EBF48E1D814CC5ED20F8037E0A79715EEF29BE32806A1D58BB7C5DA76F550AA3D8A1FBFF0EB19CCB1A313D55CDA56C9EC2EF29632387FE8D76E3C0468043E8F663F4860EE12BF2D5B0B7474D6E694F91E6DCC4024FFFFFFFFFFFFFFFF")
+	prime = []byte(os.Getenv("PRIME"))
 	base  = big.NewInt(2)
 )
 
@@ -166,50 +169,21 @@ func (s *Server) handle_connection(conn net.Conn) {
 	s.generatePrivateKey()
 	s.generatePublicKey()
 	s.performKeyExchange(conn)
-	shell_mode := false
-	prompt := "\nGoGo*⨠ "
 	for {
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print(prompt)
-		cmd, err := reader.ReadString('\n')
-		CheckError(err)
-		command := strings.TrimSpace(string(cmd))
-		args := strings.Split(command, " ")
-		if shell_mode {
-			if args[0] == "quit" {
-				shell_mode = false
-				prompt = "\nGoGo*⨠ "
-			}
-			s.send(conn, COMMAND, command)
-			_, data := s.receive(conn)
-			fmt.Println(data)
+		action, message := s.receive(conn)
+		fmt.Println(message)
+		if action == CONNECT {
+			s.send(conn, CONNECT, "Connected!")
 			continue
 		}
-		if args[0] == "quit" {
-			break
-		}
-		if args[0] == "cd" {
-			s.send(conn, CHANGE_DIR, args[1])
-			_, cwd := s.receive(conn)
-			prompt = cwd + "> "
+		if action == DISCONNECT {
+			s.send(conn, DISCONNECT, "")
 			continue
 		}
-		if args[0] == "shell" {
-			s.send(conn, COMMAND, args[0])
-			_, cwd := s.receive(conn)
-			prompt = cwd + "> "
-			shell_mode = true
-			continue
+		if action == MESSAGE {
+			s.send(conn, RESPONSE, message)
 		}
-		if args[0] == "run" {
-			s.send(conn, COMMAND, command)
-			_, data := s.receive(conn)
-			fmt.Println(data)
-			continue
-		}
-		fmt.Println("command '" + command + "' not recognized")
 	}
-
 }
 
 func (s *Server) accept_connections() {
@@ -230,9 +204,9 @@ func (s *Server) list_connections() {
 func (s *Server) start() {
 	fmt.Println("Starting server...")
 	var err error
-	s.Socket, err = net.Listen("tcp", "192.168.1.5:4444")
+	s.Socket, err = net.Listen("tcp", s.Host+":"+s.Port)
 	CheckError(err)
-	fmt.Println("Server listening onport 8000.")
+	fmt.Println("Server listening onport "+s.Port+"!")
 	fmt.Println("Waiting for inoming connections...")
 }
 
@@ -256,13 +230,16 @@ func (s *Server) run_dashboard() {
 }
 
 func main() {
-
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	name, err := os.Hostname()
 	CheckError(err)
 	server := Server{
 		HostName: name,
-		Host:     "192.168.1.5",
-		Port:     "4444",
+		Host:     os.Getenv("HOST"),
+		Port:     os.Getenv("PORT"),
 	}
 	server.start()
 	go server.accept_connections()
